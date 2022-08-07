77b72b7292d4ec77ca38bdc9b12fbdb8955843b8
Hugo Franco Rodrigues de Lima <hugo.rlima8@live.com>
2022-08-07 18:23:21 -0300

    pokedex - css update

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -2,24 +2,21 @@
 const baseUrl = 'https://pokeapi.co/api/v2/pokemon/';
 
 //Get Elements --------------------------------------------
+const getElement = document.querySelector.bind(document);
 const searchInput = getElement('.search-input'),
       searchButton = getElement('.search-button'),
-      container = getElement('.container'),
-      errorMessage = getElement('.error');
+      container = getElement('.pokemon'),
+      erroMessage = getElement('.error'); 
 
 var pokeName, // Name or ID searched
     pokemon, // Receives the data returned from the API
     card; // HTML returned
 
+//Functions ----------------------------------------------------
     
 // Request Function --------------------------------------------
-// This fuction reduce the code for get the HTML Elements
-function getElement(element){
-    return document.querySelector(element);
-}
-
-function requestPokeInfo(url, name){
-    fetch(url + name)
+async function requestPokeInfo(url, name){
+    await fetch(url + name)
         .then(response => response.json())
         .then(data => {
             pokemon = data;
@@ -27,16 +24,15 @@ function requestPokeInfo(url, name){
         .catch(err => console.log(err));
 }
 
-// This function build the HTML showed;
+// This function build the HTML showed --------------------------------------------
 function createCard () {
     card = `
       <div class="pokemon-picture">
         <img src="${pokemon.sprites.front_default}" alt="Sprite of ${pokemon.name}">
       </div>
       <div class="pokemon-info">
-          <h1 class="name">Name: ${pokemon.name}</h1>
-          <h2 class="number">Nº ${pokemon.id}</h2>
-          <h3 class="type">Type: ${pokemon.types.map(item => ' ' + item.type.name).toString()}</h3>
+          <h1 class="name">${pokemon.name}</h1>
+          <font class="number">Nº ${pokemon.id} - Type: ${pokemon.types.map(item => ' ' + item.type.name).toString()}</font>
           <h3 class="skill">Skills: ${pokemon.moves.map(item => ' ' + item.move.name).toString()}</h3>
           <h3 class="weight">Weight: ${pokemon.weight  / 10}kg</h3>
           <h3 class="height">Height: ${pokemon.height  / 10}m</h3>
@@ -44,31 +40,30 @@ function createCard () {
     return card;
   }
 
-// This will start de app and call the methods;
-function startApp(pokeName){
-    requestPokeInfo(baseUrl, pokeName);
+// This will start de app and call the methods -------------------------------------------------------
+async function startApp(pokeName){
+    await requestPokeInfo(baseUrl, pokeName);
 
-    // The function 'createCard()' will only start 2 seconds after the startApp function execution; 
-    setTimeout(function(){
-        if (pokemon.detail){
-            errorMessage.style.display = 'block';
-            container.style.display = 'none';
-        }else{
-            errorMessage.style.display = 'none';
-            container.style.display = 'flex';
-            container.innerHTML = createCard();
-        }
-    }, 2000)
+    if (pokemon.detail){
+        erroMessage.style.display = 'block';
+        container.style.display = 'none';
+    }else{
+        erroMessage.style.display = 'none';
+        container.style.display = 'flex';
+        container.innerHTML = createCard();
+    }
 }
 
-
+//Add Events -----------------------------------------------------------------------------------------
 searchButton.addEventListener('click', event => {
     event.preventDefault();
     pokeName = searchInput.value.toLowerCase();
+    searchInput.value = '';
     startApp(pokeName);
     container.classList.add('fade');
 
+    // Reset the fade efecct - remove the fade class--------------------------------------------------
     setTimeout(() => {
         container.classList.remove('fade');
-    }, 3000);
+    }, 2000);
 });
